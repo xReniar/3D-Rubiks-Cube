5b8bb34efeea381bbbf9cd70c2889292c7b0282d
xReniar <ilaorainer@gmail.com>
2024-05-21 23:48:13 +0200

    old compile.sh with vulkan command added

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+g++ -std=c++17 -O2 \
+    main.cpp \
+    -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi \
+    src/cube/Side.cpp \
+    src/cube/Cube.cpp \
+    src/solver/Solver.cpp \
+    -o main
