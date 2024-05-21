9cbf43081e851ac52823b274e4f6c3c655298235
xReniar <ilaorainer@gmail.com>
2024-05-21 23:46:42 +0200

    added Vulkan libraries for test

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
+#define GLFW_INCLUDE_VULKAN
+#include<GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include<glm/vec4.hpp>
+#include<glm/mat4x4.hpp>
+
+
 #include"src/Rubiks.hpp"
 
 int main(){
     Cube cube;
     Solver solver;
 
-    solver.solve(cube);
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    cube.turn("UDFRBL");
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    /*
-    for(int i = 0;i < 1;i++){
-        cube.turn("R2L2F2B2U2D2");
-        //cube.show();
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
     }
-    */
-    cube.show();
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    //solver.solve(cube);
+
+    //cube.turn("UDFRBL");
     return 0;
 }
