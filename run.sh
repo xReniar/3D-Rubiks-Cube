58b6eaf7d712f7386424a882e005671754b79ec4
xReniar <ilaorainer@gmail.com>
2024-07-19 10:57:31 +0200

    added CubeGUI.cpp and moved the library flags to the end of compilation

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 g++ -std=c++17 -O2 \
     main.cpp \
-    -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi \
     src/cube/Side.cpp \
     src/cube/Cube.cpp \
     src/solver/Solver.cpp \
-    -o main ; ./main
+    src/gui/CubeGUI.cpp \
+    -o main -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi ; ./main
