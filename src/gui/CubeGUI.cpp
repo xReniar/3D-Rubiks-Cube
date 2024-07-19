304326fedfbb6e504cfafb3d2d595eb0f59428ce
xReniar <ilaorainer@gmail.com>
2024-07-19 14:18:44 +0200

    started to implement basic function

--- src/gui/CubeGUI.cpp
+++ src/gui/CubeGUI.cpp
@@ -9,15 +9,41 @@ void CubeGUI::run(){
 
 void CubeGUI::initWindow() {
     glfwInit();
-
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 }
 
 void CubeGUI::initVulkan() {
+    createInstance();
+}
 
+void CubeGUI::createInstance(){
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Rubik's Cube";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (result != VK_SUCCESS) {
+        exit(1);
+    }   
 }
 
 void CubeGUI::mainLoop() {
@@ -27,49 +53,7 @@ void CubeGUI::mainLoop() {
 }
 
 void CubeGUI::cleanup() {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
-
     glfwTerminate();
 }
-
-/*
-class RubiksCube {
-public:
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
-
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
-
-private:
-    GLFWwindow* window;
-    void initWindow() {
-        glfwInit();
-
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
-
-    void initVulkan() {
-
-    }
-
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
-
-    void cleanup() {
-        glfwDestroyWindow(window);
-
-        glfwTerminate();
-    }
-};
-*/
