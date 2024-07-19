42ea9d0b6d072cfe95e7c67ac01c5af6310ad3e5
xReniar <ilaorainer@gmail.com>
2024-07-19 10:56:43 +0200

    removed vulkan libraries and main.cpp now calls rubikscube gui trough run()

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#define GLFW_INCLUDE_VULKAN
-#include<GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include<glm/vec4.hpp>
-#include<glm/mat4x4.hpp>
-
-
 #include"src/Rubiks.hpp"
 
 int main(){
     Cube cube;
     Solver solver;
 
+    CubeGUI rubikscube;
+
+    try {
+        rubikscube.run();
+    } catch (const std::exception& e) {
+        return EXIT_FAILURE;
+    }
+
+    /*
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
@@ -31,6 +31,7 @@ int main(){
     glfwDestroyWindow(window);
 
     glfwTerminate();
+    */
 
     //solver.solve(cube);
 
