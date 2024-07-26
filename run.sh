2832316300dd3ba31b879aaf5cbc575b30f987c3
xReniar <ilaorainer@gmail.com>
2024-07-26 18:03:51 +0200

    added the command for compiling shaders

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!bin/bash
+
+# declaring path variables
+BIN_PATH="/usr/local/bin"
+
+# compling shaders using glslc
+${BIN_PATH}/glslc shaders/shader.vert -o shaders/shader.vert.spv
+${BIN_PATH}/glslc shaders/shader.frag -o shaders/shader.frag.spv
+
+# compile project
 g++ -std=c++17 -O2 \
     main.cpp \
     src/cube/Side.cpp \
     src/cube/Cube.cpp \
     src/solver/Solver.cpp \
     src/gui/CubeGUI.cpp \
-    -o main -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi ; ./main
+    -o main -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
+
+# execute project
+./main
