9c9969d1bc54def56ff54b6b8f3e68a41c8ec938
xReniar <ilaorainer@gmail.com>
2024-07-26 18:02:46 +0200

    renamed rubikscube to gui

--- main.cpp
+++ main.cpp
@@ -4,34 +4,16 @@ int main(){
     Cube cube;
     Solver solver;
 
-    CubeGUI rubikscube;
+    CubeGUI gui;
 
     try {
-        rubikscube.run();
+        gui.run();
     } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 
-    /*
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-    */
+    return EXIT_SUCCESS;
 
     //solver.solve(cube);
 
