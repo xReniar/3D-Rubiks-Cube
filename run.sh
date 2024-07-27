9304f12069cb444739bea56305897458ba658795
xReniar <ilaorainer@gmail.com>
2024-07-27 23:52:24 +0200

    updated compile command

--- run.sh
+++ run.sh
@@ -10,17 +10,9 @@ ${BIN_PATH}/glslc shaders/shader.frag -o shaders/shader.frag.spv
 # compile project
 g++ -std=c++17 -O2 \
     main.cpp \
-    src/cube/Side.cpp \
-    src/cube/Cube.cpp \
+    src/cube/*.cpp \
     src/solver/Solver.cpp \
-    src/gui/CubeGUI.cpp \
-    src/gui/ValidationLayers.cpp \
-    src/gui/PhysicalDevices.cpp \
-    src/gui/LogicalDevice.cpp \
-    src/gui/WindowSurface.cpp \
-    src/gui/SwapChain.cpp \
-    src/gui/ImageViews.cpp \
-    src/gui/GraphicsPipeline.cpp \
+    src/gui/*.cpp \
     -o main -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
 
 # execute project
