455176ea9985e32923f0817fdabd79e2e741d543
xReniar <ilaorainer@gmail.com>
2024-07-28 00:02:17 +0200

    starting all over again

--- src/gui/CubeGUI.cpp
+++ src/gui/CubeGUI.cpp
@@ -1,121 +1,18 @@
 #include"CubeGUI.hpp"
 
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
-    if (func != nullptr)
-        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    else
-        return VK_ERROR_EXTENSION_NOT_PRESENT;
-}
-
-void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
-    if (func != nullptr) func(instance, debugMessenger, pAllocator);
-}
-
-void CubeGUI::run(){
+CubeGUI::CubeGUI(int w, int h, std::string name): width{w}, height{h}, windowName{name} {
     this->initWindow();
-    this->initVulkan();
-    this->mainLoop();
-    this->cleanup();
+}
+
+CubeGUI::~CubeGUI(){
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
 
 void CubeGUI::initWindow(){
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    this->window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Rubik's Cube", nullptr, nullptr);
-}
 
-void CubeGUI::initVulkan(){
-    this->createInstance();
-    this->setupDebugMessenger();
-    this->createSurface();
-    this->pickPhysicalDevice();
-    this->createLogicalDevice();
-    this->createSwapChain();
-    this->createImageViews();
-}
-
-void CubeGUI::mainLoop(){
-    while (!glfwWindowShouldClose(this->window))
-        glfwPollEvents();
-}
-
-void CubeGUI::cleanup(){
-    for(auto imageView : this->swapChainImageViews)
-        vkDestroyImageView(this->device, imageView, nullptr);
-
-    vkDestroySwapchainKHR(this->device, this->swapChain, nullptr);
-    vkDestroyDevice(this->device, nullptr);
-
-    if(enableValidationLayers) 
-        DestroyDebugUtilsMessengerEXT(this->instance, this->debugMessenger, nullptr);
-        
-    vkDestroySurfaceKHR(this->instance, this->surface, nullptr);
-    vkDestroyInstance(this->instance, nullptr);
-    glfwDestroyWindow(this->window);
-    glfwTerminate();
-}
-
-void CubeGUI::createInstance() {
-    if (enableValidationLayers && !(this->checkValidationLayerSupport()))
-        throw std::runtime_error("validation layers requested, but not available!");
-
-    VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Rubik's Cube";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
-
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-
-    auto extensions = this->getRequiredExtensions();
-    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-    createInfo.ppEnabledExtensionNames = extensions.data();
-
-    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
-    if (enableValidationLayers) {
-        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-        createInfo.ppEnabledLayerNames = validationLayers.data();
-        this->populateDebugMessengerCreateInfo(debugCreateInfo);
-        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
-    } else {
-        createInfo.enabledLayerCount = 0;
-        createInfo.pNext = nullptr;
-    }
-
-    if (vkCreateInstance(&createInfo, nullptr, &(this->instance)) != VK_SUCCESS)
-        throw std::runtime_error("failed to create instance!");
-}
-
-void CubeGUI::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo){
-    createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-    createInfo.pfnUserCallback = this->debugCallback;
-}
-
-bool CubeGUI::checkDeviceExtensionSupport(VkPhysicalDevice device){
-    uint32_t extensionCount;
-    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
-
-    std::vector<VkExtensionProperties> availableExtensions(extensionCount);
-    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
-
-    std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
-    for (const auto& extension : availableExtensions)
-        requiredExtensions.erase(extension.extensionName);
-
-    return requiredExtensions.empty();
-}
-
-VKAPI_ATTR VkBool32 VKAPI_CALL CubeGUI::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData){
-    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
-    return VK_FALSE;
+    window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
 }
