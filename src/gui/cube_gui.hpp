4aa6f313bb9ef7098e09ded325c61ada023788a0
xReniar <ilaorainer@gmail.com>
2024-07-29 00:45:40 +0200

    added getExtent function

--- src/gui/cube_gui.hpp
+++ src/gui/cube_gui.hpp
@@ -23,6 +23,7 @@ public:
     CubeGUI &operator=(const CubeGUI &) = delete;
 
     bool shouldClose() { return glfwWindowShouldClose(window); }
+    VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
     void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
 private:
     const int width;
