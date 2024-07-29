26f6616cd8147316378c30a9d3e5d2059cf73627
xReniar <ilaorainer@gmail.com>
2024-07-29 11:30:37 +0200

    creating command buffers and drawing frames, also waiting the device to get idle

--- src/gui/cube_app.cpp
+++ src/gui/cube_app.cpp
@@ -1,6 +1,7 @@
 #include"cube_app.hpp"
 
 #include<stdexcept>
+#include<array>
 
 CubeApp::CubeApp(){
     createPipelineLayout();
@@ -15,7 +16,10 @@ CubeApp::~CubeApp(){
 void CubeApp::run(){
     while(!cubeGUI.shouldClose()){
         glfwPollEvents();
+        drawFrame();
     }
+
+    vkDeviceWaitIdle(device.device());
 }
 
 void CubeApp::createPipelineLayout(){
@@ -43,9 +47,58 @@ void CubeApp::createPipeline(){
 }
 
 void CubeApp::createCommandBuffers(){
+    commandBuffers.resize(swapChain.imageCount());
 
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandPool = device.getCommandPool();
+    allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+    if(vkAllocateCommandBuffers(device.device(), & allocInfo, commandBuffers.data()) != VK_SUCCESS)
+        throw std::runtime_error("failed to create command buffers!");
+
+    for(int i = 0;i < commandBuffers.size(); i++){
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+        if(vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS)
+            throw std::runtime_error("failed to begin recording command buffer!");
+
+        VkRenderPassBeginInfo renderPassInfo{};
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        renderPassInfo.renderPass = swapChain.getRenderPass();
+        renderPassInfo.framebuffer = swapChain.getFrameBuffer(i);
+
+        renderPassInfo.renderArea.offset = {0,0};
+        renderPassInfo.renderArea.extent = swapChain.getSwapChainExtent();
+
+        std::array<VkClearValue, 2> clearValues{};
+        clearValues[0].color = { 0.1f, 0.1f, 0.1f, 0.1f};
+        clearValues[1].depthStencil = { 1.0f, 0 };           // getting error on narrowing conversion, changed from 0.0f to 0
+        renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+        renderPassInfo.pClearValues = clearValues.data();
+
+        vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+        pipeline -> bind(commandBuffers[i]);
+        // this records a draw command to draw 3 vertices and 1 instance
+        vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+        vkCmdEndRenderPass(commandBuffers[i]);
+        if(vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS)
+            throw std::runtime_error("failed to record command buffer!");
+    }
 }
 
 void CubeApp::drawFrame(){
+    uint32_t imageIndex;
+    auto result = swapChain.acquireNextImage(&imageIndex);
 
+    if(result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
+        throw std::runtime_error("failed to create swap chain image");
+
+    result = swapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+    if(result != VK_SUCCESS)
+        throw std::runtime_error("failed to present swap chain image!");
 }
