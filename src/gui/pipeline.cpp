fa166905bdb5dab9edbf7511400ddbe09f5d7369
xReniar <ilaorainer@gmail.com>
2024-07-29 11:29:15 +0200

    added bind function for command buffer

--- src/gui/pipeline.cpp
+++ src/gui/pipeline.cpp
@@ -20,6 +20,10 @@ Pipeline::~Pipeline(){
     vkDestroyPipeline(device.device(), graphicsPipeline, nullptr);
 }
 
+void Pipeline::bind(VkCommandBuffer commandBuffer){
+    vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+}
+
 PipelineConfigInfo Pipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height){
     PipelineConfigInfo configInfo{};
     configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
