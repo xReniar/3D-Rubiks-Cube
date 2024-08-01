333ba8c5c0923b19f860680a24fcf3e61b1140be
xReniar <ilaorainer@gmail.com>
2024-08-01 15:26:12 +0200

    the depth is now considered in the swap chain

--- src/gui/swap_chain.cpp
+++ src/gui/swap_chain.cpp
@@ -280,6 +280,7 @@ void SwapChain::createFramebuffers() {
 
 void SwapChain::createDepthResources() {
     VkFormat depthFormat = findDepthFormat();
+    swapChainDepthFormat = depthFormat;
     VkExtent2D swapChainExtent = getSwapChainExtent();
 
     depthImages.resize(imageCount());
--- src/gui/swap_chain.hpp
+++ src/gui/swap_chain.hpp
@@ -35,6 +35,10 @@ public:
 
     VkResult acquireNextImage(uint32_t *imageIndex);
     VkResult submitCommandBuffers(const VkCommandBuffer *buffers, uint32_t *imageIndex);
+    bool compareSwapFormats(const SwapChain& swapChain) const {
+        return swapChain.swapChainDepthFormat == swapChainDepthFormat && 
+               swapChain.swapChainImageFormat == swapChainImageFormat;
+    }
 
 private:
     void createSwapChain();
@@ -51,6 +55,7 @@ private:
     VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
 
     VkFormat swapChainImageFormat;
+    VkFormat swapChainDepthFormat;
     VkExtent2D swapChainExtent;
 
     std::vector<VkFramebuffer> swapChainFramebuffers;
