80cd563bf11c3910f42edfebe4302cb694b221b7
xReniar <ilaorainer@gmail.com>
2024-08-01 15:26:27 +0200

    implemented the renderer stage

---
+++ src/gui/renderer.cpp
@@ -0,0 +1,139 @@
+#include"renderer.hpp"
+
+#include<cassert>
+#include<stdexcept>
+#include<array>
+
+
+Renderer::Renderer(CubeGUI &cubeGui,Device &device): cubeGUI{cubeGUI}, device{device} {
+    recreateSwapChain();
+    createCommandBuffers();
+}
+
+Renderer::~Renderer(){
+    freeCommandBuffers();
+}
+
+void Renderer::createCommandBuffers(){
+    commandBuffers.resize(SwapChain::MAX_FRAMES_IN_FLIGHT);
+
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandPool = device.getCommandPool();
+    allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+    if(vkAllocateCommandBuffers(device.device(), & allocInfo, commandBuffers.data()) != VK_SUCCESS)
+        throw std::runtime_error("failed to create command buffers!");
+}
+
+void Renderer::recreateSwapChain(){
+    auto extent = cubeGUI.getExtent();
+    while(extent.width == 0 || extent.height == 0){
+        extent = cubeGUI.getExtent();
+        glfwWaitEvents();
+    }
+
+    vkDeviceWaitIdle(device.device());
+
+    if(swapChain == nullptr){
+        swapChain = std::make_unique<SwapChain>(device, extent);
+    } else {
+        std::shared_ptr<SwapChain> oldSwapChain = std::move(swapChain);
+        swapChain = std::make_unique<SwapChain>(device, extent, oldSwapChain);
+
+        if(!oldSwapChain->compareSwapFormats(*swapChain.get()))
+            throw std::runtime_error("Swap chain image(or depth) format has changed");
+    }
+}
+
+void Renderer::freeCommandBuffers(){
+    vkFreeCommandBuffers(
+        device.device(),
+        device.getCommandPool(),
+        static_cast<uint32_t>(commandBuffers.size()),
+        commandBuffers.data()
+    );
+    commandBuffers.clear();
+}
+
+VkCommandBuffer Renderer::beginFrame(){
+    assert(!isFrameStarted && "Can't call beginFrame while already in progress");
+    auto result = swapChain->acquireNextImage(&currentImageIndex);
+
+    if(result == VK_ERROR_OUT_OF_DATE_KHR){
+        recreateSwapChain();
+        return nullptr;
+    }
+
+    if(result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
+        throw std::runtime_error("failed to create swap chain image");
+
+    isFrameStarted = true;
+    auto commandBuffer = getCurrentCommandBuffer();
+
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+    if(vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS)
+        throw std::runtime_error("failed to begin recording command buffer!");
+    return commandBuffer;
+}
+
+void Renderer::endFrame(){
+    assert(isFrameStarted && "Can't call endFrame while frame is not in progress");
+    auto commandBuffer = getCurrentCommandBuffer();
+    if(vkEndCommandBuffer(commandBuffer) != VK_SUCCESS)
+        throw std::runtime_error("failed to record command buffer!");
+
+    auto result = swapChain->submitCommandBuffers(&commandBuffer, &currentImageIndex);
+    if(result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || cubeGUI.wasWindowResized()){
+        cubeGUI.resetWindowResizedFlag();
+        recreateSwapChain();
+    } else if(result != VK_SUCCESS){
+        throw std::runtime_error("failed to present swap chain image!");
+    }
+
+    isFrameStarted = false;
+    currentFrameIndex = (currentFrameIndex + 1) % SwapChain::MAX_FRAMES_IN_FLIGHT;
+}
+
+void Renderer::beginSwapChainRenderPass(VkCommandBuffer commandBuffer){
+    assert(isFrameStarted && "Can't call endFrame while frame is not in progress");
+    assert(commandBuffer == getCurrentCommandBuffer() && "Can't begin render pass on command buffer from a different frame");
+
+    VkRenderPassBeginInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = swapChain->getRenderPass();
+    renderPassInfo.framebuffer = swapChain->getFrameBuffer(currentImageIndex);
+
+    renderPassInfo.renderArea.offset = {0,0};
+    renderPassInfo.renderArea.extent = swapChain->getSwapChainExtent();
+
+    std::array<VkClearValue, 2> clearValues{};
+    clearValues[0].color = { 0.01f, 0.01f, 0.01f, 0.1f};
+    clearValues[1].depthStencil = { 1.0f, 0 };           // getting error on narrowing conversion, changed from 0.0f to 0
+    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+    renderPassInfo.pClearValues = clearValues.data();
+
+    vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = static_cast<float>(swapChain->getSwapChainExtent().width);
+    viewport.height = static_cast<float>(swapChain->getSwapChainExtent().height);
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+    VkRect2D scissor{{0, 0}, swapChain->getSwapChainExtent()};
+    vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+    vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+}
+
+void Renderer::endSwapChainRenderPass(VkCommandBuffer commandBuffer){
+    assert(isFrameStarted && "Can't call endFrame while frame is not in progress");
+    assert(commandBuffer == getCurrentCommandBuffer() && "Can't begin render pass on command buffer from a different frame");
+
+    vkCmdEndRenderPass(commandBuffer);
+}
---
+++ src/gui/renderer.hpp
@@ -0,0 +1,52 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include"cube_gui.hpp"
+#include"swap_chain.hpp"
+#include"device.hpp"
+#include"model.hpp"
+
+#include<memory>
+#include<vector>
+#include<cassert>
+
+class Renderer {
+public:
+    Renderer(CubeGUI &cubeGUI, Device &device);
+    ~Renderer();
+
+    Renderer(const Renderer &) = delete;
+    Renderer &operator=(const Renderer &) = delete;
+
+    VkRenderPass getSwapChainRenderPass() const { return swapChain->getRenderPass(); }
+    bool isFrameInProgress() const { return isFrameStarted; }
+    VkCommandBuffer getCurrentCommandBuffer() const {
+        assert(isFrameStarted && "Cannot get command buffer when frame not in progress");
+        return commandBuffers[currentFrameIndex]; 
+    }
+
+    int getFrameIndex() const {
+        assert(isFrameStarted && "Cannot get frame index when frame not in progress");
+        return currentFrameIndex;
+    }
+
+    VkCommandBuffer beginFrame();
+    void endFrame();
+    void beginSwapChainRenderPass(VkCommandBuffer commandBuffer);
+    void endSwapChainRenderPass(VkCommandBuffer commandBuffer);
+private:
+    CubeGUI &cubeGUI;
+    Device &device;
+    std::unique_ptr<SwapChain> swapChain;
+    std::vector<VkCommandBuffer> commandBuffers;
+    uint32_t currentImageIndex = 0;
+    int currentFrameIndex;
+    bool isFrameStarted = false;
+
+    void createCommandBuffers();
+    void recreateSwapChain();
+    void freeCommandBuffers();
+
+};
+
+#endif
