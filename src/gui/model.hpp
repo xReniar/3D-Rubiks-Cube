b258ba2b1b17f3bc53dbc2640b9b27702af8c05c
xReniar <ilaorainer@gmail.com>
2024-08-03 16:36:14 +0200

    implemented staging and index buffer

--- src/gui/model.hpp
+++ src/gui/model.hpp
@@ -16,7 +16,13 @@ public:
         static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
         static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
     };
-    Model(Device& device, const std::vector<Vertex> &vertices);
+
+    struct Builder {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+    };
+
+    Model(Device& device, const Model::Builder &builder);
     ~Model();
 
     Model(const Model &) = delete;
@@ -26,11 +32,18 @@ public:
     void draw(VkCommandBuffer commandBuffer);
 private:
     Device& device;
+
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
     uint32_t vertexCount;
 
+    bool hasIndexBuffer = false;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+    uint32_t indexCount;
+
     void createVertexBuffers(const std::vector<Vertex> &vertices);
+    void createIndexBuffers(const std::vector<uint32_t> &indices);
 };
 
 #endif
