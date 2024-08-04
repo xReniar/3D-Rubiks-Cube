c0a74fbe33e450a77c03e5696e3cad861fa298ff
xReniar <ilaorainer@gmail.com>
2024-08-05 00:25:34 +0200

    added variables for target rotation, current rotation, movestate, added keys for rotation, and separated orbital and rotational operation

--- src/gui/controller.hpp
+++ src/gui/controller.hpp
@@ -4,6 +4,24 @@
 #include"cube_obj.hpp"
 #include"cube_gui.hpp"
 
+struct TargetRotation {
+    float U_turn = 0.0f;
+    float D_turn = 0.0f;
+    float F_turn = 0.0f;
+    float R_turn = 0.0f;
+    float B_turn = 0.0f;
+    float L_turn = 0.0f;
+};
+
+struct CurrentRotation {
+    float U_turn = 0.0f;
+    float D_turn = 0.0f;
+    float F_turn = 0.0f;
+    float R_turn = 0.0f;
+    float B_turn = 0.0f;
+    float L_turn = 0.0f;
+};
+
 class Controller {
 public:
     struct KeyMappings {
@@ -17,13 +35,43 @@ public:
         int lookRight = GLFW_KEY_RIGHT;
         int lookUp = GLFW_KEY_UP;
         int lookDown = GLFW_KEY_DOWN;
+
+        int inverse = GLFW_KEY_0;
+        int u_turn = GLFW_KEY_1;
+        int d_turn = GLFW_KEY_2;
+        int f_turn = GLFW_KEY_3;
+        int r_turn = GLFW_KEY_4;
+        int b_turn = GLFW_KEY_5;
+        int l_turn = GLFW_KEY_6;
     };
+    
+    struct MoveState {
+        bool U_turn = false;
+        bool D_turn = false;
+        bool F_turn = false;
+        bool R_turn = false;
+        bool B_turn = false;
+        bool L_turn = false;
+    };
+    
 
     KeyMappings keys{};
     float moveSpeed{3.f};
     float lookSpeed{1.5f};
 
-    void moveInPlaneXZ(GLFWwindow* window, float dt, CubeObj& cubeObj);
+    float rotationSpeed = glm::radians(360.0f);
+    float turnAngle = glm::radians(90.0f);
+    float inverse = 1.f;
+
+    bool inverseKeyPressed = false;
+
+    MoveState moves{};
+    TargetRotation target{};
+    CurrentRotation current{};
+
+    void orbitAroundCube(GLFWwindow* window, float dt, CubeObj& viewerObject);
+    void rotateCube(GLFWwindow* window, float dt, std::vector<CubeObj> &gameObjects);
+    
 };
 
 #endif
