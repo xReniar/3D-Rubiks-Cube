d2c2e3d9293a8a52ca5fa916505c47699e941eea
xReniar <ilaorainer@gmail.com>
2024-08-05 00:24:27 +0200

    corrected rotation and implemented some moves

--- src/gui/controller.cpp
+++ src/gui/controller.cpp
@@ -1,8 +1,11 @@
 #include"controller.hpp"
 
+#include<iostream>
+
+#include<glm/gtc/epsilon.hpp>
 #include<limits>
 
-void Controller::moveInPlaneXZ(GLFWwindow* window, float dt, CubeObj& cubeObj){
+void Controller::orbitAroundCube(GLFWwindow* window, float dt, CubeObj& viewerObject){
     glm::vec3 rotate{0};
     if (glfwGetKey(window, keys.lookRight) == GLFW_PRESS) rotate.y += 1.f;
     if (glfwGetKey(window, keys.lookLeft) == GLFW_PRESS) rotate.y -= 1.f;
@@ -10,14 +13,14 @@ void Controller::moveInPlaneXZ(GLFWwindow* window, float dt, CubeObj& cubeObj){
     if (glfwGetKey(window, keys.lookDown) == GLFW_PRESS) rotate.x -= 1.f;
 
     if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon()) {
-        cubeObj.transform.rotation += lookSpeed * dt * glm::normalize(rotate);
+        viewerObject.transform.rotation += lookSpeed * dt * glm::normalize(rotate);
     }
 
     // limit pitch values between about +/- 85ish degrees
-    cubeObj.transform.rotation.x = glm::clamp(cubeObj.transform.rotation.x, -1.5f, 1.5f);
-    cubeObj.transform.rotation.y = glm::mod(cubeObj.transform.rotation.y, glm::two_pi<float>());
+    viewerObject.transform.rotation.x = glm::clamp(viewerObject.transform.rotation.x, -1.5f, 1.5f);
+    viewerObject.transform.rotation.y = glm::mod(viewerObject.transform.rotation.y, glm::two_pi<float>());
 
-    float yaw = cubeObj.transform.rotation.y;
+    float yaw = viewerObject.transform.rotation.y;
     const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
     const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
     const glm::vec3 upDir{0.f, -1.f, 0.f};
@@ -31,6 +34,73 @@ void Controller::moveInPlaneXZ(GLFWwindow* window, float dt, CubeObj& cubeObj){
     if (glfwGetKey(window, keys.moveDown) == GLFW_PRESS) moveDir -= upDir;
 
     if (glm::dot(moveDir, moveDir) > std::numeric_limits<float>::epsilon()) {
-        cubeObj.transform.translation += moveSpeed * dt * glm::normalize(moveDir);
+        viewerObject.transform.translation += moveSpeed * dt * glm::normalize(moveDir);
+    }
+}
+
+void Controller::rotateCube(GLFWwindow* window, float dt, std::vector<CubeObj> &gameObjects){
+    // change moves from clockwise to anticlockwise or viceversa
+    if(glfwGetKey(window, keys.inverse) == GLFW_PRESS) {
+        inverseKeyPressed = true;
+    }
+
+    // Controlla se il tasto inverse è stato rilasciato
+    if(glfwGetKey(window, keys.inverse) == GLFW_RELEASE && inverseKeyPressed) {
+        inverse *= -1.0f;
+        inverseKeyPressed = false;
+    }
+
+    if(glfwGetKey(window, keys.r_turn) == GLFW_PRESS && !moves.R_turn){
+        target.R_turn -= (turnAngle * inverse);
+        moves.R_turn = true;
+    }
+
+    if(glfwGetKey(window, keys.u_turn) == GLFW_PRESS && !moves.U_turn){
+        target.U_turn += (turnAngle * inverse);
+        moves.U_turn = true;
+    }
+
+    if(glfwGetKey(window, keys.d_turn) == GLFW_PRESS && !moves.D_turn){
+        target.D_turn -= (turnAngle * inverse);
+        moves.D_turn = true;
+    }
+    
+
+    if (moves.R_turn) {
+        current.R_turn = glm::mix(current.R_turn, target.R_turn, rotationSpeed * dt);
+
+        if (glm::epsilonEqual(current.R_turn, target.R_turn, 0.01f)) {
+            current.R_turn = target.R_turn;
+            moves.R_turn = false;
+        }
+
+        auto& obj = gameObjects[1];
+        auto& obj2 = gameObjects[2];
+        obj.transform.rotation.x = current.R_turn;
+        obj2.transform.rotation.x = current.R_turn;
+    }
+
+    if (moves.U_turn) {
+        current.U_turn = glm::mix(current.U_turn, target.U_turn, rotationSpeed * dt);
+
+        if (glm::epsilonEqual(current.U_turn, target.U_turn, 0.01f)) {
+            current.U_turn = target.U_turn;
+            moves.U_turn = false;
+        }
+
+        auto& obj2 = gameObjects[2];
+        obj2.transform.rotation.y = current.U_turn;
+    }
+
+    if (moves.D_turn) {
+        current.D_turn = glm::mix(current.D_turn, target.D_turn, rotationSpeed * dt);
+
+        if (glm::epsilonEqual(current.D_turn, target.D_turn, 0.01f)) {
+            current.D_turn = target.D_turn;
+            moves.D_turn = false;
+        }
+
+        auto& obj2 = gameObjects[2];
+        obj2.transform.rotation.y = current.D_turn;
     }
 }
