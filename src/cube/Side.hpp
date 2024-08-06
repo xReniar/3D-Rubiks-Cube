7cd3016f989cf1cb045394f64197d25355647035
xReniar <ilaorainer@gmail.com>
2024-08-07 01:36:47 +0200

    modified Side to Cubie, to make it easier to manage piece's id

--- src/cube/Side.hpp
+++ src/cube/Side.hpp
@@ -2,12 +2,18 @@
 #define SIDE_H
 
 #include"../constants.hpp"
+#include<vector>
+
+struct Cubie {
+    int id;
+    Color color;
+};
 
 class Side {
 public:
-    Color slot[9];
-    Side(Color color);
-    Side(Color colors[]);
+    Cubie slot[9];
+    Side(Color color, std::vector<int> pieceId);
+    Side(Color colors[], std::vector<int> pieceId);
     void turn(Direction direction);
 };
 
