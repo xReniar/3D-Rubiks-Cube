7ad2b2f50d19cd0afdc568e10562180b976fd794
xReniar <ilaorainer@gmail.com>
2024-08-10 13:39:29 +0200

    implemented rotate function

--- src/gui/cube_obj.cpp
+++ src/gui/cube_obj.cpp
@@ -9,8 +9,12 @@ void CubeObj::adjustAxis(std::string rotateSide, float angle) {
     if(rotateSide == "LEFT") transform.coordinateSystem.rotate('x', angle);
 }
 
-void CubeObj::rotate(char plane, float value){
+void CubeObj::rotate(char plane, float dt){
     char correctPlane = plane;
+    //current.R_turn = glm::mix(current.R_turn, target.R_turn, rotationSpeed * dt);
+    if(plane == 'x') transform.rotation.x = glm::mix(transform.rotation.x, transform.targetRotation.x, rotationSpeed * dt);
+    if(plane == 'y') transform.rotation.y = glm::mix(transform.rotation.y, transform.targetRotation.y, rotationSpeed * dt);
+    if(plane == 'z') transform.rotation.z = glm::mix(transform.rotation.z, transform.targetRotation.z, rotationSpeed * dt);
     /*
     for(const auto& pair : transform.axis)
         if(pair.second.value == plane)
