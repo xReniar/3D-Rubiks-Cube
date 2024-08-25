7ef92ddfec5e19061b21e6b4e60428bca9c824b5
xReniar <ilaorainer@gmail.com>
2024-08-26 00:50:57 +0200

    implemented coordSystem and added quatRotation variabile with respective mat4 function

--- src/gui/cube_obj.hpp
+++ src/gui/cube_obj.hpp
@@ -7,30 +7,55 @@
 #include<glm/gtc/matrix_transform.hpp>
 #include<memory>
 #include<map>
+#include <tuple>
+
+struct CoordSystem {
+    glm::vec3 i{1.0f, 0.0f, 0.0f};
+    glm::vec3 j{0.0f, 1.0f, 0.0f};
+    glm::vec3 k{0.0f, 0.0f, 1.0f};
+
+    glm::mat4 rotationMatrix{1.f};
+
+    glm::vec3 approximate(const glm::vec3& v) {
+        const float epsilon = 1e-6f;
+        return glm::vec3(
+            std::abs(v.x) < epsilon ? 0.0f : v.x,
+            std::abs(v.y) < epsilon ? 0.0f : v.y,
+            std::abs(v.z) < epsilon ? 0.0f : v.z
+        );
+    }
+
+    void rotate(char plane, float angle){
+        if(plane == 'x') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(1.0f, 0.0f, 0.0f));
+        if(plane == 'y') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
+        if(plane == 'z') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+
+        i = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(i, 0.0f))));
+        j = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(j, 0.0f))));
+        k = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(k, 0.0f))));
+    }
+
+    std::tuple<char, int> getAxis(char axis) {
+        glm::vec3 vec;
+        std::tuple<char, int> res;
+        if (axis == 'x') vec = i;
+        if (axis == 'y') vec = j;
+        if (axis == 'z') vec = k;
+
+        if (vec.x != 0.0f) { std::get<0>(res) = 'x'; std::get<1>(res) = int(vec.x); }
+        if (vec.y != 0.0f) { std::get<0>(res) = 'y'; std::get<1>(res) = int(vec.y); }
+        if (vec.z != 0.0f) { std::get<0>(res) = 'z'; std::get<1>(res) = int(vec.z); }
+
+        return res;
+    }
+};
 
 struct TransformComponent {
     glm::vec3 translation{};
     glm::vec3 scale{ 1.f, 1.f, 1.f };
-    glm::vec3 rotation{};
-
-    glm::mat4 mat4(){
-        // Crea matrici di rotazione separate per ogni asse
-        /*
-        glm::mat4 rotX = glm::rotate(glm::mat4(1.0f), rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-        glm::mat4 rotY = glm::rotate(glm::mat4(1.0f), rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-        glm::mat4 rotZ = glm::rotate(glm::mat4(1.0f), rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
-
-        // Combina le rotazioni nell'ordine Z * Y * X per rotazioni globali
-        glm::mat4 rotationMatrix = rotY * rotX * rotZ;
-
-        // Crea matrici di scala e traslazione
-        glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
-        glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translation);
-
-        // Combina tutte le trasformazioni
-        // L'ordine è: scala, poi rotazione, poi traslazione
-        return translationMatrix * rotationMatrix * scaleMatrix;
-        */
+    glm::vec3 rotation{}; // for camera
+    CoordSystem coordSystem{};
+    glm::mat4 mat4_camera(){
         const float c3 = glm::cos(rotation.z);
         const float s3 = glm::sin(rotation.z);
         const float c2 = glm::cos(rotation.y);
@@ -59,6 +84,18 @@ struct TransformComponent {
             },
             {translation.x, translation.y, translation.z, 1.0f}
         };
+    }
+
+    glm::quat quatRotation{1.0f, 0.0f, 0.0f, 0.0f};; // for cube
+    glm::mat4 mat4(){
+        glm::mat4 transform = glm::mat4(1.0f);
+
+        transform = glm::translate(transform, translation);
+        transform = transform * glm::mat4_cast(quatRotation);
+        transform = glm::scale(transform, scale);
+
+        return transform;
+
     }
 };
 
