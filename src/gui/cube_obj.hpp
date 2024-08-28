f9724e8074ec911708af6680cff67e8cb8a9a2d3
xReniar <ilaorainer@gmail.com>
2024-08-28 17:33:54 +0200

    the function are now implemented in the cube_obj.cpp

--- src/gui/cube_obj.hpp
+++ src/gui/cube_obj.hpp
@@ -14,40 +14,9 @@ struct CoordSystem {
     glm::vec3 j{0.0f, 1.0f, 0.0f};
     glm::vec3 k{0.0f, 0.0f, 1.0f};
 
-    glm::mat4 rotationMatrix{1.f};
-
-    glm::vec3 approximate(const glm::vec3& v) {
-        const float epsilon = 1e-6f;
-        return glm::vec3(
-            std::abs(v.x) < epsilon ? 0.0f : v.x,
-            std::abs(v.y) < epsilon ? 0.0f : v.y,
-            std::abs(v.z) < epsilon ? 0.0f : v.z
-        );
-    }
-
-    void rotate(char plane, float angle){
-        if(plane == 'x') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(1.0f, 0.0f, 0.0f));
-        if(plane == 'y') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 1.0f, 0.0f));
-        if(plane == 'z') rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
-
-        i = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(i, 0.0f))));
-        j = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(j, 0.0f))));
-        k = glm::normalize(approximate(glm::vec3(rotationMatrix * glm::vec4(k, 0.0f))));
-    }
-
-    std::tuple<char, int> getAxis(char axis) {
-        glm::vec3 vec;
-        std::tuple<char, int> res;
-        if (axis == 'x') vec = i;
-        if (axis == 'y') vec = j;
-        if (axis == 'z') vec = k;
-
-        if (vec.x != 0.0f) { std::get<0>(res) = 'x'; std::get<1>(res) = int(vec.x); }
-        if (vec.y != 0.0f) { std::get<0>(res) = 'y'; std::get<1>(res) = int(vec.y); }
-        if (vec.z != 0.0f) { std::get<0>(res) = 'z'; std::get<1>(res) = int(vec.z); }
-
-        return res;
-    }
+    glm::vec3 approximate(const glm::vec3& v);
+    void rotate(char plane, float angle);
+    std::tuple<char, int> getAxis(char axis);
 };
 
 struct TransformComponent {
