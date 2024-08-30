05b00583e668575b3ee2f91d3f384e62cc5263ca
xReniar <ilaorainer@gmail.com>
2024-08-30 17:35:15 +0200

    finished solver implementation

--- src/solver/Solver.cpp
+++ src/solver/Solver.cpp
@@ -1,7 +1,38 @@
 #include"Solver.hpp"
 
+Solver::Solver(){
+    Py_Initialize();
+    PySys_SetPath(L".:/usr/lib/python3.10");
+    PyRun_SimpleString("import sys; sys.path.append('./src/solver/'); print(sys.path);");
+    //PyRun_SimpleString("import os, sys; sys.path.append(os.path.abspath('./src/solver')); print(sys.path)");
+}
+
+Solver::~Solver(){
+    freeMemory();
+} 
+
 void Solver::kociemba(const char* cubeState){
-    std::string solution = "";
-    std::cout << cubeState << std::endl;
+    // loading solver module
+    name = PyUnicode_FromString((char *)"kociemba");
+    load_module = PyImport_Import(name);
+
+    // getting function that returns solution
+    func = PyObject_GetAttrString(load_module, (char*)"main");
+
+    // creating arguments and passing to main function
+    args = PyTuple_Pack(1, PyUnicode_FromString(cubeState));
+    callfunc = PyObject_CallObject(func, args);
+
+    std::string solution = PyUnicode_AsUTF8(callfunc);
     this->solution = solution;
+
+    freeMemory();
+}
+
+void Solver::freeMemory(){
+    Py_DECREF(name);
+    Py_DECREF(load_module);
+    Py_DECREF(func);
+    Py_DECREF(args);
+    Py_DECREF(callfunc);
 }
