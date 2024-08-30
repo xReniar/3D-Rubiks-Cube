8844a0da71a4e301fac818c0624a139c83110840
xReniar <ilaorainer@gmail.com>
2024-08-30 22:32:03 +0200

    added function that returns the best solve

--- src/solver/kociemba/__init__.py
+++ src/solver/kociemba/__init__.py
@@ -1,5 +1,23 @@
 from . import solve
+import time
+
+def solve_best_generator(cube_state, max_length=25, max_time=10):
+    sm = solve.SolutionManager(cube_state)
+    timeout = time.time() + max_time
+    while True:
+        solution = sm.solve(max_length, timeout)
+
+        if(isinstance(solution, str)):
+            yield solution
+            max_length = len(solution.split(" ")) - 1
+        elif solution == -2 or solution == -1:
+            break
+        else:
+            raise RuntimeError(
+                f"SolutionManager.solve: unexpected return value {solution}"
+            )    
 
 def main(cube_state:str):
-    sm = solve.SolutionManager(cube_state)
-    return sm.solve()
+    solutions = list(solve_best_generator(cube_state, max_length=25, max_time=10))
+    solutions.sort()
+    return solutions[0]
