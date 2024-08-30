031edf3636815f2af403bf2c29dd3ffce2696064
xReniar <ilaorainer@gmail.com>
2024-08-30 17:35:35 +0200

    added constructor and PyObject, also added freeMemory function

--- src/solver/Solver.hpp
+++ src/solver/Solver.hpp
@@ -2,10 +2,16 @@
 #define SOLVER_H
 
 #include"../cube/Cube.hpp"
+#include<python3.10/Python.h>
 
 class Solver {
 public:
+    Solver();
+    ~Solver();
     std::string solution;
+    PyObject *name, *load_module, *func, *callfunc, *args;
+
+    void freeMemory();
     void kociemba(const char* cubeState);
 };
 
