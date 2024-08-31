649d9d488b99441851ee5808acbdc5fdf1aef618
xReniar <ilaorainer@gmail.com>
2024-08-31 18:56:13 +0200

    changed solution from string to vector of string, added also solutionReady status variable

--- src/solver/Solver.hpp
+++ src/solver/Solver.hpp
@@ -3,12 +3,14 @@
 
 #include"../cube/Cube.hpp"
 #include<python3.10/Python.h>
+#include<sstream>
 
 class Solver {
 public:
     Solver();
     ~Solver();
-    std::string solution;
+    std::vector<std::string> solution;
+    bool solutionReady = false;
     PyObject *name, *load_module, *func, *callfunc, *args;
 
     void freeMemory();
